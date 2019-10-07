b5922c75dd43749565380bf1638b8aee10be985a
Juan Pedraza <51345057+JuanPedraza96@users.noreply.github.com>
2019-10-06 22:12:26 -0300

    Main Menu Juego

--- PrimatonG4/Form1.Designer.cs
+++ PrimatonG4/Form1.Designer.cs
@@ -28,139 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblTituloExtra = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnJugar = new System.Windows.Forms.Button();
-            this.lblTitulo = new System.Windows.Forms.Label();
             this.lblExit = new System.Windows.Forms.Label();
-            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.pnlBot = new System.Windows.Forms.Panel();
+            this.txtJugador = new System.Windows.Forms.TextBox();
             this.lblDni = new System.Windows.Forms.Label();
-            this.txtDni = new System.Windows.Forms.TextBox();
-            this.lblContrasenia = new System.Windows.Forms.Label();
-            this.btnRegistrarse = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pboxLogo = new System.Windows.Forms.PictureBox();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.pnlTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblTituloExtra
-            // 
-            this.lblTituloExtra.AutoSize = true;
-            this.lblTituloExtra.BackColor = System.Drawing.Color.Transparent;
-            this.lblTituloExtra.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTituloExtra.Location = new System.Drawing.Point(53, 74);
-            this.lblTituloExtra.Name = "lblTituloExtra";
-            this.lblTituloExtra.Size = new System.Drawing.Size(357, 29);
-            this.lblTituloExtra.TabIndex = 7;
-            this.lblTituloExtra.Text = "informacion adicional o subtitulo";
             // 
             // btnJugar
             // 
-            this.btnJugar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnJugar.Location = new System.Drawing.Point(285, 419);
+            this.btnJugar.BackColor = System.Drawing.Color.Black;
+            this.btnJugar.FlatAppearance.BorderSize = 0;
+            this.btnJugar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJugar.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 38.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJugar.ForeColor = System.Drawing.Color.Yellow;
+            this.btnJugar.Location = new System.Drawing.Point(125, 408);
             this.btnJugar.Name = "btnJugar";
-            this.btnJugar.Size = new System.Drawing.Size(125, 38);
+            this.btnJugar.Size = new System.Drawing.Size(209, 68);
             this.btnJugar.TabIndex = 6;
             this.btnJugar.Text = "JUGAR";
-            this.btnJugar.UseVisualStyleBackColor = true;
-            
-            // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(112, 20);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(249, 31);
-            this.lblTitulo.TabIndex = 1;
-            this.lblTitulo.Text = "Titulo del Programa";
+            this.btnJugar.UseVisualStyleBackColor = false;
             // 
             // lblExit
             // 
             this.lblExit.AutoSize = true;
             this.lblExit.BackColor = System.Drawing.Color.Transparent;
-            this.lblExit.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblExit.Location = new System.Drawing.Point(429, 9);
+            this.lblExit.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.ForeColor = System.Drawing.Color.Yellow;
+            this.lblExit.Location = new System.Drawing.Point(435, 7);
             this.lblExit.Name = "lblExit";
-            this.lblExit.Size = new System.Drawing.Size(29, 31);
+            this.lblExit.Size = new System.Drawing.Size(32, 35);
             this.lblExit.TabIndex = 0;
             this.lblExit.Text = "X";
             this.lblExit.Click += new System.EventHandler(this.LblExit_Click);
             // 
-            // txtContrasenia
+            // pnlTitulo
             // 
-            this.txtContrasenia.Location = new System.Drawing.Point(243, 317);
-            this.txtContrasenia.Name = "txtContrasenia";
-            this.txtContrasenia.PasswordChar = '*';
-            this.txtContrasenia.Size = new System.Drawing.Size(100, 20);
-            this.txtContrasenia.TabIndex = 11;
+            this.pnlTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(70)))));
+            this.pnlTitulo.Controls.Add(this.lblExit);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(470, 50);
+            this.pnlTitulo.TabIndex = 14;
+            // 
+            // pnlBot
+            // 
+            this.pnlBot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(70)))));
+            this.pnlBot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlBot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBot.Location = new System.Drawing.Point(0, 505);
+            this.pnlBot.Name = "pnlBot";
+            this.pnlBot.Size = new System.Drawing.Size(470, 15);
+            this.pnlBot.TabIndex = 15;
+            // 
+            // txtJugador
+            // 
+            this.txtJugador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(59)))), ((int)(((byte)(104)))));
+            this.txtJugador.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtJugador.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJugador.ForeColor = System.Drawing.Color.Gold;
+            this.txtJugador.Location = new System.Drawing.Point(163, 329);
+            this.txtJugador.Name = "txtJugador";
+            this.txtJugador.Size = new System.Drawing.Size(238, 20);
+            this.txtJugador.TabIndex = 10;
+            this.txtJugador.Text = "JUGADOR";
+            this.txtJugador.Enter += new System.EventHandler(this.TxtJugador_Enter);
+            this.txtJugador.Leave += new System.EventHandler(this.TxtJugador_Leave);
             // 
             // lblDni
             // 
             this.lblDni.AutoSize = true;
             this.lblDni.BackColor = System.Drawing.Color.Transparent;
-            this.lblDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDni.Location = new System.Drawing.Point(101, 256);
+            this.lblDni.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDni.ForeColor = System.Drawing.Color.White;
+            this.lblDni.Location = new System.Drawing.Point(12, 323);
             this.lblDni.Name = "lblDni";
-            this.lblDni.Size = new System.Drawing.Size(53, 25);
+            this.lblDni.Size = new System.Drawing.Size(143, 33);
             this.lblDni.TabIndex = 9;
-            this.lblDni.Text = "DNI:";
+            this.lblDni.Text = "Jugador:";
             // 
-            // txtDni
+            // pboxLogo
             // 
-            this.txtDni.Location = new System.Drawing.Point(243, 262);
-            this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(100, 20);
-            this.txtDni.TabIndex = 10;
+            this.pboxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pboxLogo.Image")));
+            this.pboxLogo.Location = new System.Drawing.Point(0, 45);
+            this.pboxLogo.Name = "pboxLogo";
+            this.pboxLogo.Size = new System.Drawing.Size(470, 231);
+            this.pboxLogo.TabIndex = 17;
+            this.pboxLogo.TabStop = false;
             // 
-            // lblContrasenia
+            // shapeContainer1
             // 
-            this.lblContrasenia.AutoSize = true;
-            this.lblContrasenia.BackColor = System.Drawing.Color.Transparent;
-            this.lblContrasenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContrasenia.Location = new System.Drawing.Point(101, 311);
-            this.lblContrasenia.Name = "lblContrasenia";
-            this.lblContrasenia.Size = new System.Drawing.Size(123, 25);
-            this.lblContrasenia.TabIndex = 8;
-            this.lblContrasenia.Text = "Contraseña";
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(470, 520);
+            this.shapeContainer1.TabIndex = 18;
+            this.shapeContainer1.TabStop = false;
             // 
-            // btnRegistrarse
+            // lineShape1
             // 
-            this.btnRegistrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrarse.Location = new System.Drawing.Point(68, 417);
-            this.btnRegistrarse.Name = "btnRegistrarse";
-            this.btnRegistrarse.Size = new System.Drawing.Size(146, 40);
-            this.btnRegistrarse.TabIndex = 12;
-            this.btnRegistrarse.Text = "Registrarse";
-            this.btnRegistrarse.UseVisualStyleBackColor = true;
-            this.btnRegistrarse.Click += new System.EventHandler(this.btnRegistrarse_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(77, 142);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "label1";
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 163;
+            this.lineShape1.X2 = 400;
+            this.lineShape1.Y1 = 351;
+            this.lineShape1.Y2 = 351;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(59)))), ((int)(((byte)(104)))));
             this.ClientSize = new System.Drawing.Size(470, 520);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnRegistrarse);
-            this.Controls.Add(this.txtContrasenia);
+            this.Controls.Add(this.pboxLogo);
+            this.Controls.Add(this.pnlBot);
+            this.Controls.Add(this.pnlTitulo);
             this.Controls.Add(this.lblDni);
-            this.Controls.Add(this.txtDni);
-            this.Controls.Add(this.lblContrasenia);
-            this.Controls.Add(this.lblExit);
+            this.Controls.Add(this.txtJugador);
             this.Controls.Add(this.btnJugar);
-            this.Controls.Add(this.lblTituloExtra);
-            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.shapeContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Juego Didactico Sierras de Cordoba";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlTitulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,14 +172,13 @@
         #endregion
         private System.Windows.Forms.Label lblExit;
         private System.Windows.Forms.Button btnJugar;
-        private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.Label lblTituloExtra;
-        private System.Windows.Forms.TextBox txtContrasenia;
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Panel pnlBot;
+        private System.Windows.Forms.TextBox txtJugador;
         private System.Windows.Forms.Label lblDni;
-        private System.Windows.Forms.TextBox txtDni;
-        private System.Windows.Forms.Label lblContrasenia;
-        private System.Windows.Forms.Button btnRegistrarse;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pboxLogo;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
     }
 }
 
--- PrimatonG4/Form1.cs
+++ PrimatonG4/Form1.cs
@@ -32,5 +32,26 @@ namespace PrimatonG4
             Registrarse.ShowDialog();
             this.Show();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BordesRedondeados.BorderRedondoButton(btnJugar);
+        }
+
+        private void TxtJugador_Enter(object sender, EventArgs e)
+        {
+            if (txtJugador.Text == "JUGADOR")
+            {
+                txtJugador.Text = "";
+            }
+        }
+
+        private void TxtJugador_Leave(object sender, EventArgs e)
+        {
+            if (txtJugador.Text == "")
+            {
+                txtJugador.Text = "JUGADOR";
+            }
+        }
     }
 }
